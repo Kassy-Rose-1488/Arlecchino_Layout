e710861b8e91260e3acf34edcb38bf564d12c1c1
syimykDev00 <syimykshailoobek@gmail.com>
2024-02-20 11:11:22 +0300

    arlecchino media header footer add

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+let body = document.querySelector(".white")
+let btn = document.querySelector(".btn-icon")
+
+btn.addEventListener("click", () => {
+    if (body.className === "white") {
+        body.classList.replace("white", "dark")
+        btn.style.rotate = "180deg"
+    } else {
+        body.classList.replace("dark", "white")
+        btn.style.rotate = "0deg"
+    }
+})
